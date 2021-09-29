f3bd784c9a5a24de7e78591ccdb434ac0c2714b8
ryantobin77 <ryantobin77@gmail.com>
2021-09-29 13:56:42 -0400

    Inititial commit and setup

---
+++ StudentSublease_iOS/StudentSublease_iOS/UI_Layer/VCs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentSublease_iOS
+//
+//  Created by Ryan Tobin on 9/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
